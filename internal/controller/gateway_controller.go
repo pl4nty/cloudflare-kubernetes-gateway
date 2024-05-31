f80b5af8dedf4fbc2c74ddc379efded848ba21e7
Tom Plant <tom@tplant.com.au>
2024-05-31 14:09:29 +0000

    fix: cloudflared version
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- internal/controller/gateway_controller.go
+++ internal/controller/gateway_controller.go
@@ -215,7 +215,7 @@ func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 				Spec: core.PodSpec{Containers: []core.Container{{
 					Name: "main",
 					// renovate: datasource=docker
-					Image: "cloudflare/cloudflared:2024.5.1",
+					Image: "cloudflare/cloudflared:2024.5.0",
 					Args:  []string{"tunnel", "--no-autoupdate", "run", "--token", token, "--metrics", "0.0.0.0:2000"},
 					LivenessProbe: &core.Probe{
 						FailureThreshold:    5,
