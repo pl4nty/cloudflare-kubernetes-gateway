1820c32b92a9ddb4dcf23525b196b127d077a9a0
Tom Plant <tom@tplant.com.au>
2024-06-20 14:45:45 +0000

    fix: move conformance args inline
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -14,6 +15,8 @@ func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
 	fmt.Fprintf(GinkgoWriter, "Starting cloudflare-kubernetes-gateway suite\n") //nolint:errcheck
 	RunSpecs(t, "e2e suite")
+
 	fmt.Fprintf(GinkgoWriter, "Starting gateway-api conformance suite\n") //nolint:errcheck
+	os.Args = []string{"noop", "--cleanup-base-resources=false", "--conformance-profiles=GATEWAY-HTTP"}
 	conformance.RunConformance(t)
 }
