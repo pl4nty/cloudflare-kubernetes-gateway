1f766d62d49e245ccec8a9f53e28b4ae211beba8
Tom Plant <tom@tplant.com.au>
2024-07-21 07:03:25 +0000

    generate and upload conformance report
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -1,13 +1,15 @@
 package e2e
 
 import (
-	"flag"
 	"fmt"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/gateway-api/conformance"
+	conformancev1 "sigs.k8s.io/gateway-api/conformance/apis/v1"
+	"sigs.k8s.io/gateway-api/conformance/utils/suite"
 )
 
 // Run e2e tests using the Ginkgo runner.
@@ -17,8 +19,19 @@ func TestE2E(t *testing.T) {
 	RunSpecs(t, "e2e suite")
 
 	fmt.Fprintf(GinkgoWriter, "Starting gateway-api conformance suite\n") //nolint:errcheck
-	if flag.Set("cleanup-base-resources", "false") != nil || flag.Set("conformance-profiles", "GATEWAY-HTTP") != nil {
-		t.Fatal("Failed to set conformance flags")
+	opts := conformance.DefaultOptions(t)
+	opts.CleanupBaseResources = false
+	opts.ConformanceProfiles = sets.New(
+		suite.GatewayHTTPConformanceProfileName,
+	)
+	opts.Debug = true
+	opts.Implementation = conformancev1.Implementation{
+		Contact:      []string{"https://github.com/pl4nty/cloudflare-kubernetes-gateway/issues/new/choose"},
+		Organization: "pl4nty",
+		Project:      "cloudflare-kubernetes-gateway",
+		URL:          "https://github.com/pl4nty/cloudflare-kubernetes-gateway",
+		Version:      "latest",
 	}
-	conformance.RunConformance(t)
+	opts.ReportOutputPath = "standard-latest-default-report.yaml"
+	conformance.RunConformanceWithOptions(t, opts)
 }
