d7a600b6e50fbbcee52987ce68c992952338148b
Tom Plant <tom@tplant.com.au>
2024-07-21 14:06:44 +0000

    fix secret
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- internal/controller/gateway_controller.go
+++ internal/controller/gateway_controller.go
@@ -321,6 +321,7 @@ func (r *GatewayReconciler) doFinalizerOperationsForGateway(ctx context.Context,
 
 		// prepare for deletion - disconnect, after rotating secret to prevent reconnect
 		if _, err := api.ZeroTrust.Tunnels.Edit(ctx, tunnel.Result[0].ID, zero_trust.TunnelEditParams{
+			AccountID:    cloudflare.String(account),
 			TunnelSecret: cloudflare.String(""),
 		}); err != nil {
 			log.Error(err, "Failed to update tunnel secret")
