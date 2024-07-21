82635bad34f25bff5ede4632e860910482436f78
Tom Plant <tom@tplant.com.au>
2024-07-21 11:02:39 +0000

    move deployments to resource namespace
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- internal/controller/gateway_controller.go
+++ internal/controller/gateway_controller.go
@@ -243,7 +243,7 @@ func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 	// Check if the deployment already exists, if not create a new one
 	found := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: "cloudflare-" + gateway.Name, Namespace: gateway.Namespace}, found)
+	err = r.Get(ctx, types.NamespacedName{Name: gateway.Name, Namespace: gateway.Namespace}, found)
 	// TODO update existing deployment eg image changes
 	if err != nil && apierrors.IsNotFound(err) {
 		// Define a new deployment
@@ -363,8 +363,8 @@ func (r *GatewayReconciler) deploymentForGateway(
 
 	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "cloudflare-" + gateway.Name,
-			Namespace: gateway.Namespace,
+			Name:      gateway.Name,
+			Namespace: r.Namespace,
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: &replicas,
