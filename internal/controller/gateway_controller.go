1fe92a0b2cecf60f44083d35c498a8ddfc7c3cdf
Tom Plant <tom@tplant.com.au>
2024-10-14 15:07:04 +1100

    Update internal/controller/gateway_controller.go

--- internal/controller/gateway_controller.go
+++ internal/controller/gateway_controller.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
